212a5a7e363d044d21f6d58f301190300b0ec845
Duja Abdurhman <134400247+DujaAbdulrhman@users.noreply.github.com>
2025-03-19 02:56:30 +0300

    Update CategoryService.java

--- src/main/java/com/example/capstone/Service/CategoryService.java
+++ src/main/java/com/example/capstone/Service/CategoryService.java
@@ -1,58 +1,5 @@
 package com.example.capstone.Service;
-/*
-import com.example.capstone.Model.Category;
-import org.springframework.stereotype.Service;
-import jakarta.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
-@Service
-public class CategoryService {
-    private List<Category> categories = new ArrayList<>();
-
-
-    public void addCategory(@Valid Category category) {
-        categories.add(category);
-    }
-
-    public List<Category> getAllCategories() {
-        return categories;
-    }
-
-
-    public Category getCategoryById(String id) {
-        for (Category category : categories) {
-            if (category.getId().equals(id)) {
-                return category;
-            }
-        }
-        return null;
-    }
-
-        public Category updateCategory(String id, @Valid Category updatedCategory) {
-        for (int i = 0; i < categories.size(); i++) {
-            if (categories.get(i).getId().equals(id)) {
-                categories.set(i, updatedCategory);
-                return updatedCategory;
-            }
-        }
-            return updatedCategory;
-        }
-
-
-    public void deleteCategory(String id) {
-        for (int i = 0; i < categories.size(); i++) {
-            if (categories.get(i).getId().equals(id)) {
-                categories.remove(i);
-                break;
-            }
-        }
-    }
-
-    public List<Category> getCategory() {
-        return categories;
-    }
-}*/
 
 import com.example.capstone.Model.Category;
 import jakarta.validation.constraints.NotNull;
